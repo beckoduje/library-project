e13444d530842b512bf7a5933b4f493ee3a1093b
beckoduje <dujebecko@gmail.com>
2021-08-18 15:39:28 +0300

    18/08/2021

--- src/components/results/ResultsList.js
+++ src/components/results/ResultsList.js
@@ -9,7 +9,25 @@ export default function ResultsList({ searchedBooks }) {
           : searchedBooks.items.map((book) => {
               return (
                 <li key={Math.random()} className="results-list-item">
-                  {book.volumeInfo.title}
+                  <h4 className="book-title">{book.volumeInfo.title}</h4>
+                  <div className="book-authors-cont">
+                    {book.volumeInfo.authors.length <= 1 ? (
+                      <span className="book-author">
+                        {book.volumeInfo.authors[0]}
+                      </span>
+                    ) : (
+                      book.volumeInfo.authors.map((author) => {
+                        return (
+                          <span key={Math.random()} className="book-author">
+                            {author}
+                          </span>
+                        );
+                      })
+                    )}
+                  </div>
+                  <div className="book-average-rating">
+                    {book.volumeInfo.averageRating}
+                  </div>
                 </li>
               );
             })}
