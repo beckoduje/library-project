48bc65d41f7183db4558d5228eb4b4fc3c75fb77
beckoduje <dujebecko@gmail.com>
2021-09-20 23:43:33 +0300

    20/09/2021

--- src/components/shared/StarRating.js
+++ src/components/shared/StarRating.js
@@ -1,8 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
+
+import { LibraryContext } from "../LibraryContext";
 
 export default function StarRating() {
+  const { singleBook } = useContext(LibraryContext);
+  const [userRatings, setUserRatings] = useState();
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+
+  const ID = !singleBook ? "1234" : singleBook.id;
+
+  let userRatingsHelper = new Set();
+
+  console.log(`${userRatings} + Stanje jebeno`);
+  console.log(`${userRatingsHelper} + helper set`);
+
+  const USER_RATING_KEY = "userRating";
+
+  useEffect(() => {
+    // -------------------------- ALL COLLECTION --------------------------
+    const userRatingJSON = localStorage.getItem(USER_RATING_KEY);
+    // if (userRatingJSON != null) setUserRatings(JSON.parse(userRatingJSON));
+    if (userRatingJSON != null) {
+      setUserRatings(JSON.parse(userRatingJSON));
+      userRatingsHelper.add(userRatings);
+    }
+  }, []);
+  // -------------------------- ALL COLLECTION --------------------------
+  useEffect(() => {
+    localStorage.setItem(USER_RATING_KEY, JSON.stringify(userRatings));
+  }, [userRatings]);
+
+  function addRatingToLS(index) {
+    setRating(index);
+    let tempRating = {
+      id: ID,
+      userRating: index,
+    };
+    userRatingsHelper.add(tempRating);
+    setUserRatings(...userRatingsHelper);
+  }
+
   return (
     <div className="star-rating">
       <p className="star-rating-text">Leave your rating:</p>
@@ -15,8 +53,17 @@ export default function StarRating() {
               key={index}
               className={
                 index <= (hover || rating) ? "star-btn on" : "star-btn off"
+                // index <= (hover || userRatings.singleBook.id)
+                //   ? "star-btn on"
+                //   : "star-btn off"
               }
-              onClick={() => setRating(index)}
+              onClick={() => addRatingToLS(index)}
+              // onClick={() =>
+              //   setUserRatings([
+              //     ...userRatings,
+              //     { id: singleBook.id, userRating: index },
+              //   ])
+              // }
               onMouseEnter={() => setHover(index)}
               onMouseLeave={() => setHover(rating)}
             >
@@ -28,14 +75,3 @@ export default function StarRating() {
     </div>
   );
 }
-
-// Array.from({ length: 5 }, (v, i) => (
-//   <i
-//     key={`star_${i + 1}`}
-//     className={
-//       i < book.volumeInfo.averageRating
-//         ? "fas fa-star start-rating full"
-//         : "fas fa-star start-rating empty"
-//     }
-//   />
-// )
