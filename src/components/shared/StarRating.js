9c041f25a583cae668289d4c04b0ba5c03d94686
beckoduje <dujebecko@gmail.com>
2021-09-24 00:28:13 +0300

    23/09/2021

--- src/components/shared/StarRating.js
+++ src/components/shared/StarRating.js
@@ -4,9 +4,11 @@ import { LibraryContext } from "../LibraryContext";
 
 export default function StarRating() {
   const { singleBook } = useContext(LibraryContext);
-  const [userRatings, setUserRatings] = useState([]);
+  // const [userRatings, setUserRatings] = useState([]); --- opet bez stanja
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+
+  let userRatings = [];
 
   /*
     console.log(`ID varijabla: ${ID}`);
@@ -22,18 +24,34 @@ export default function StarRating() {
       userRating: index,
     };
     // setUserRatings([...userRatings].filter((book) => book.id !== ID));
-    [...userRatings].forEach((book) => console.log(book.id));
-    console.log(`Iz dodavanja funckije ${[...userRatings]}`);
-    setUserRatings([...userRatings, tempRating]);
-    console.log(userRatings);
+    // setUserRatings([...userRatings, tempRating]); ----------------- opet bez stanja -----------------
+    // setUserRatings(userRatings.add(tempRating));
+    // console.log(userRatings); ----------------- opet bez stanja -----------------
+    userRatings.filter((book) => book.id !== ID);
+    userRatings.push(tempRating);
+    localStorage.setItem(USER_RATING_KEY, JSON.stringify(userRatings));
   }
+
+  // useEffect(() => { // ----------------- opet bez stanja -----------------
+  //   const userRatingsJSON = localStorage.getItem(USER_RATING_KEY);
+  //   if (userRatingsJSON != null) {
+  //     setUserRatings(JSON.parse(userRatingsJSON));
+  //     console.log(`Iz GET use Effekta ${userRatings}`);
+  //   }
+  // }, []);
 
   useEffect(() => {
     const userRatingsJSON = localStorage.getItem(USER_RATING_KEY);
     if (userRatingsJSON != null) {
-      setUserRatings(JSON.parse(userRatingsJSON));
+      userRatings.push(JSON.parse(userRatingsJSON));
     }
   }, []);
+
+  // const userRatingsJSON = localStorage.getItem(USER_RATING_KEY); ----- s ovim se normalno array loguje ali onda imam one nestane arraya kad dodajem (i kad je ovaj gornji iskomentiran)
+  // if (userRatingsJSON != null) {
+  //   userRatings.push(JSON.parse(userRatingsJSON));
+  //   console.log(userRatings);
+  // }
 
   useEffect(() => {
     if (userRatings.length > 0) {
@@ -45,6 +63,29 @@ export default function StarRating() {
   useEffect(() => {
     ID = !singleBook ? "1234" : singleBook.id;
   });
+
+  // useEffect(() => {
+  //   console.log(`User rating iz zadnjeg UseEffecta: ${userRatings}`);
+  //   if (userRatings.some((book) => book.id === ID)) {
+  //     console.log("naddjeno");
+  //   } else {
+  //     console.log("nije nadeo");
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    console.log(`User rating iz zadnjeg UseEffecta: ${userRatings}`);
+    if (userRatings.some((book) => book.id === ID)) {
+      console.log("naddjeno");
+    } else {
+      console.log("nije nadeo");
+    }
+  }, []);
+
+  /*E ne ide ti onda push
+Nego
+userRatings= userRatingsJSON
+Jer ti ovako pokusavas array da pushujes u array*/
 
   return (
     <div className="star-rating">
