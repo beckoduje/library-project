6bb52c36abdf2fe22f1b4d195fb5c086c24ee946
beckoduje <dujebecko@gmail.com>
2021-10-19 20:48:32 +0300

    19/10/2021

--- src/components/shared/Loading.js
+++ src/components/shared/Loading.js
@@ -3,18 +3,15 @@ import React from "react";
 import { loadAnimation } from "lottie-web";
 import { defineLordIconElement } from "lord-icon-element";
 
-import book from "../../images/bookAnima.gif";
-
 export default function Loading() {
   // register lottie and define custom element
   defineLordIconElement(loadAnimation);
   return (
     <div className="loading-container">
-      {/* <lord-icon
-        trigger="hover"
-        src="/522-fish-outline.json"
-      ></lord-icon> */}
-      <img src={book} />
+      <lord-icon
+        trigger="loop"
+        src="/112-book-morph-outline-edited.json"
+      ></lord-icon>
     </div>
   );
 }
